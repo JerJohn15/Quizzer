eb9e24c1195d7efdcfd27d92c2246e2b27f164c7
JerJohn15 <tjon2013@bu.edu>
2015-03-26 11:09:52 -0400

    Removed create quiz and copyright css
    removed div for copyright paragraph and create quiz.

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <?php ?>
 <div class="footer">
-    <p class='copyright'>&copy; 2014 - CS473, Software Engineering Group 3</p>
+<!-- @Jeremiah Removed copyright class from css -->
+    <p >&copy; 2014 - CS473, Software Engineering Group 3</p>
 </div>
 </body>
 </html>
